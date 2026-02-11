ec5d487a19d7143f563cc9f1612e49c6bca45331
ThoHuj <nyheterna-vid.0v@icloud.com>
2026-02-11 21:09:08 +0100

    Setting up Tailwind.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import mdx from '@mdx-js/rollup'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     { enforce: "pre", ...mdx() },
-    react()
+    react(),
+    tailwindcss()
   ],
 })
