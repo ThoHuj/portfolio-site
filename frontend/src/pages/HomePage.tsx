ea5d90d19a9a233902502b9863dcc0d0f793e901
ThoHuj <nyheterna-vid.0v@icloud.com>
2026-02-11 22:03:44 +0100

    Moving from App to main & adding a HomePage component

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import HeaderContainer from "../components/Header.tsx"
+import ContentArea from "../components/ContentArea.tsx"
+import Article from "../components/Article.tsx"
+import NavbarContainer from "../components/Navbar.tsx"
+import GameDevPost from "../assets/GameDevPost.mdx"
+
+const article = <GameDevPost />
 
 function HomePage() {
     return (
-        <div>HomePage</div>
+        <div><HeaderContainer title="Thom Hujanen">
+            <NavbarContainer />
+        </HeaderContainer>
+            <ContentArea>
+                <Article
+                    imageurl="https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/695050/capsule_616x353.jpg?t=1572876316"
+                    imagealt="Promo image of the videogame 'Rain of Reflections: Set free'"
+                    children={article}>
+                </Article>
+            </ContentArea></div>
     )
 }
 
