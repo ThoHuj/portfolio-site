a75be4f6eb6cd182d9a324c383c4311073219dcb
ThoHuj <nyheterna-vid.0v@icloud.com>
2026-02-12 20:48:22 +0100

    Adjusting subheading.

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -6,7 +6,7 @@ function HomePage() {
         <div className="p-1 flex flex-col items-center text-gray-200">
             <header className="min-w-[80vw] flex flex-col items-center">
                 <h1 className="p-2 text-center text-5xl">Thom Hujanen</h1>
-                <h2 className="pb-2 w-full border-b border-white/30 text-nowrap text-center" >~ a portfolio ~</h2>
+                <h2 className="pb-2 w-full border-b border-white/30 text-nowrap text-center" >~ a revolutionary device ~</h2>
             </header>
             <main className="w-screen py-6 flex flex-col items-center
             bg-[linear-gradient(90deg,transparent_10%,rgba(0,0,0,0.15)_40%,rgba(0,0,0,0.15)_60%,transparent_90%)]
