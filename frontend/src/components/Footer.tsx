f788cce94b16f0bdd1fdd983bda37c527982fc60
ThoHuj <nyheterna-vid.0v@icloud.com>
2026-02-13 00:29:04 +0100

    Making Footer buttons wrap.

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import NavButton from "./NavButton"
 
 function Footer() {
   return (
-    <div className="space-x-4 pt-6 min-w-[80vw] border-t border-white/30 flex justify-center items-center gap-2">
+    <div className="pt-6 min-w-[80vw] border-t border-white/30 flex justify-center items-center gap-4 flex-wrap ">
         <NavButton text="GitHub" url="https://github.com/ThoHuj/"/>
         <NavButton text="LinkedIn" url="https://www.linkedin.com/in/thohuj/"/>
         <NavButton text="Instagram" url="https://www.instagram.com/thomomom/"/>
