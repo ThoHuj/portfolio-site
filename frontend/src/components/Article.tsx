30d0c9c5abf8bb4397d85dfa8746c24e5cd7542f
ThoHuj <nyheterna-vid.0v@icloud.com>
2026-02-12 01:48:48 +0100

    Adding border to Article.

--- frontend/src/components/Article.tsx
+++ frontend/src/components/Article.tsx
@@ -8,8 +8,8 @@ type ArticleProps = {
 
 export default function Article({ imageurl, imagealt, children }: ArticleProps) {
     return (
-        <div className="max-w-150 mt-2 flex flex-col items-center">
-            <img className="rounded-2xl" src={imageurl} alt={imagealt}></img>
+        <div className="max-w-150 mt-2 flex flex-col items-center border-b rounded-2xl">
+            <img className="border-b rounded-2xl" src={imageurl} alt={imagealt}></img>
             <div className="p-2 prose-h2:text-2xl prose-h2:py-4 prose-ul:list-disc prose-ul:list-inside prose-ul:space-y-2 prose-ul:p-4">{children}</div>
         </div>
     );
