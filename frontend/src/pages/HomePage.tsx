83fe6c0748280e8d056e267ff3946d6ed4ddb140
ThoHuj <nyheterna-vid.0v@icloud.com>
2026-02-13 13:05:29 +0100

    Adding random subheadings.

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,22 +3,29 @@ import Footer from '../components/Footer.tsx'
 import { useState } from 'react'
 import ArticleRoR from '../articles/ArticleRoR.tsx';
 
-
+const titletexts: string[] = [
+    "a revolutionary device",
+    "this changes everything",
+    "it just works",
+    "incredibly fast"
+]
+const randomSubheading = titletexts[Math.floor(Math.random() * titletexts.length)];
 
 function HomePage() {
     const [mainContent, setMainContent] = useState("CardBar");
+
 
     return (
         <div className="p-1 flex flex-col items-center text-gray-200 font-normal">
             <header className="min-w-[80vw] flex flex-col items-center">
                 <button onClick={() => setMainContent("CardBar")} className="min-w-[80vw] cursor-pointer">
                     <h1 className="p-2 text-center text-5xl">Thom Hujanen</h1>
-                    <h2 className="pb-2 w-full border-b border-white/30 text-nowrap text-center" >~ a revolutionary device ~</h2>
+                    <h2 className="pb-2 w-full border-b border-white/30 text-nowrap text-center" >~ {randomSubheading} ~</h2>
                 </button>
             </header>
             <main className="w-screen py-6 flex flex-col items-center
                              bg-[linear-gradient(90deg,transparent_10%,rgba(0,0,0,0.15)_40%,rgba(0,0,0,0.15)_60%,transparent_90%)]">
-                {mainContent === "CardBar" && <CardBar onNavigate={setMainContent}/>}
+                {mainContent === "CardBar" && <CardBar onNavigate={setMainContent} />}
                 {mainContent === "Article" && <ArticleRoR />}
             </main>
             <footer className="">
