6990b89ebb1cdd54218a13e7770e37190a54ec47
ThoHuj <nyheterna-vid.0v@icloud.com>
2026-02-19 21:24:54 +0100

    Making CardBar construct cards with data fetched from API.

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,36 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+
 app = FastAPI()
 
 origins = [
     "http://localhost:3000",
+    "http://127.0.0.1:3000",
     "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
+
+card_data = [{
+            "title": "Fullstack",
+            "summary": "Educated in fullstack & DevOps development. Skilled in designing systems, databases, APIs & deployment of web applications.",
+            "imageSrc": "https://i.redd.it/a1ebq74mcl181.png",
+            "targetUrl": "/fullstack",
+            },
+            {
+            "title": "AI & ML",
+            "summary": "Studying implementaton and deployment of artificial intelligence & machine learning systems.",
+            "imageSrc": "https://cdn.i-scmp.com/sites/default/files/styles/700x400/public/d8/images/canvas/2025/07/10/0355f1da-90cd-40ec-a5a9-b96670a2aa8b_d08379d6.jpg?itok=9RzHlip3&v=1752155786",
+            "targetUrl": "/AiMl",
+            },
+            {
+            "title": "Game Dev",
+            "summary": "Bachelor's degree in Game Design with over 5 years working in the industry.",
+            "imageSrc": "https://a.storyblok.com/f/158607/1920x1080/4ddf5af627/egs_goatsimulator3_theshadiestupdate_banner1920x1080-notext.jpg/m/fit-in/600x600",
+            "targetUrl": "/GameDev",
+            },
+            ]
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -16,22 +40,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-"""
-@app.get("/")
-def root():
-    return {"message":"This text is fetched from the API."}
-"""
-
-@app.get("/carddata/")
-def list_card_data():
-    return [{"title": "a card's title",
-            "summary": "A card's summary",
-            "imageSrc": "https://veganfeministnetwork.com/wp-content/uploads/2015/10/multiple-women-laughing-with-salad.jpg",
-            "imageAlt": "A cards header image alt text",
-            },
-            {"title": "a second card's title",
-            "summary": "A second card's summary",
-            "imageSrc": "https://veganfeministnetwork.com/wp-content/uploads/2015/10/multiple-women-laughing-with-salad.jpg",
-            "imageAlt": "A second cards header image alt text",
-            },
-            ]
+@app.get("/cards")
+def list_cards(category_id: int | None = None):
+    return card_data
--- frontend/src/components/CardBar.tsx
+++ frontend/src/components/CardBar.tsx
@@ -5,7 +5,7 @@ export default function CardBar() {
   const [cards, setCards] = useState([]);
 
   async function fetchCardData(parentCategoryName: string) {
-    const endpoint = "http://localhost:8000/carddata/" + parentCategoryName
+    const endpoint = "http://localhost:8000/cards" + parentCategoryName
     try {
       const response = await fetch(endpoint);
       const data = await response.json();
@@ -16,30 +16,14 @@ export default function CardBar() {
     }
   };
 
-
   useEffect(() => {
     fetchCardData("");
   }, []);
 
   return (
     <div className="flex gap-6 flex-wrap justify-center">
-      <CategoryCard
-        targetUrl="/fullstack"
-        title="Fullstack"
-        imageUrl="https://i.redd.it/a1ebq74mcl181.png"
-        body="Educated in fullstack & DevOps development. Skilled in designing systems, databases, APIs & deployment of web applications." />
-      <CategoryCard
-        targetUrl="/AiMl"
-        title="AI & ML"
-        imageUrl="https://cdn.i-scmp.com/sites/default/files/styles/700x400/public/d8/images/canvas/2025/07/10/0355f1da-90cd-40ec-a5a9-b96670a2aa8b_d08379d6.jpg?itok=9RzHlip3&v=1752155786"
-        body="Studying implementaton and deployment of artificial intelligence & machine learning systems." />
-      <CategoryCard
-        targetUrl="/GameDev"
-        title="Game Dev"
-        imageUrl="https://a.storyblok.com/f/158607/1920x1080/4ddf5af627/egs_goatsimulator3_theshadiestupdate_banner1920x1080-notext.jpg/m/fit-in/600x600"
-        body="Bachelor's degree in Game Design with over 5 years working in the industry." />
       {cards.map((card) => <CategoryCard
-        targetUrl="/GameDev"
+        targetUrl={card.targetUrl}
         title={card.title}
         imageUrl={card.imageSrc}
         body={card.summary} />)
